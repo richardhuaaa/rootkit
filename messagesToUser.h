d5dac8464a457353ef33ac119fb5c91f142e9c1e
Adam Black <>
2013-06-05 15:14:44 +1000

    added ability to make rootkit silent

--- messagesToUser.h
+++ messagesToUser.h
@@ -5,5 +5,13 @@
 // these are included here to ease disabling printing
 // macros are used over functions to help work out the actual source of error if printf is called with the wrong arguments in the end
 
-#define printInfo(format, ...) printk(KERN_INFO format, ##__VA_ARGS__)
-#define printError(format, ...) printk(KERN_ERR format, ##__VA_ARGS__)
+
+#define BE_SILENT 0
+
+#if BE_SILENT
+	#define printInfo(format, ...)
+	#define printError(format, ...)
+#else
+	#define printInfo(format, ...) printk(KERN_INFO format, ##__VA_ARGS__)
+	#define printError(format, ...) printk(KERN_ERR format, ##__VA_ARGS__)
+#endif
