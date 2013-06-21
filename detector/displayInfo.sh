27ba1cb911bb69e9edc45aecb67fb66f12f6e351
Adam Black <>
2013-06-21 12:11:11 +1000

    added looking at dmesg

--- detector/displayInfo.sh
+++ detector/displayInfo.sh
@@ -6,3 +6,5 @@ echo "/proc/modules: `grep blank /proc/modules`"
 echo "/sys/module: `ls /sys/module | grep blank`"
 
 echo "/proc/kallsyms: `fgrep "[blank]" /proc/kallsyms | head -n1`"
+
+echo "dmesg: `dmesg | tail`"
